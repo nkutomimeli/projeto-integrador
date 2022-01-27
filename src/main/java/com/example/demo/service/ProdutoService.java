a33b3e479f529339fe61c2cb5327e0969f13cdb9
Lucian Moraes <lucian.moraes@mercadolivre.com.br>
2022-01-27 15:22:29 -0300

    Iniciando a feature Mock de Dados

---
+++ src/main/java/com/example/demo/service/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Produto;
+import com.example.demo.repository.ProdutoRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public void save(Produto produto){
+        this.produtoRepository.save(produto);
+    }
+
+}
