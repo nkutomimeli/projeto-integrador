0b7633fa8af436fc027706cb4a02929ad2e55746
VanessaGomesOliveira <vanessa.goliveira@mercadolivre.com>
2022-01-27 14:35:11 -0300

    correcao repositorys

--- src/main/java/com/example/demo/repository/AnuncioRepository.java
+++ src/main/java/com/example/demo/repository/AnuncioRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Produto;
+import com.example.demo.entity.Anuncio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository <Produto, Long> {
+public interface AnuncioRepository extends JpaRepository <Anuncio, Long> {
 }
--- src/main/java/com/example/demo/repository/OrdemEntradaRepository.java
+++ src/main/java/com/example/demo/repository/OrdemEntradaRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Produto;
+import com.example.demo.entity.OrdemEntrada;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository <Produto, Long> {
+public interface OrdemEntradaRepository extends JpaRepository <OrdemEntrada, Long> {
 }
