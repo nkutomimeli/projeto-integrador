20f23d6c138393c691ef985a828a9eb362f62872
VanessaGomesOliveira <vanessa.goliveira@mercadolivre.com>
2022-01-27 11:27:48 -0300

    correcao ProdutoRepository

--- src/main/java/com/example/demo/repository/ProdutoRepository.java
+++ src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.produto;
+import com.example.demo.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository //corrigir classe produto ****
-public interface ProdutoRepository extends JpaRepository <produto, Long> {
+public interface ProdutoRepository extends JpaRepository <Produto, Long> {
 }
