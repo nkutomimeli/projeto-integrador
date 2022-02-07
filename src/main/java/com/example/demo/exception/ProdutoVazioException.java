7df1fa863a4fef65ee9858db671db4dcfaf1ab34
VanessaGomesOliveira <vanessa.goliveira@mercadolivre.com>
2022-02-07 11:17:45 -0300

    Finalizacao do Requisito US04

--- src/main/java/com/example/demo/exception/ProdutoVazioException.java
+++ src/main/java/com/example/demo/exception/ProdutoVazioException.java
@@ -2,7 +2,7 @@ package com.example.demo.exception;
 
 public class ProdutoVazioException extends RuntimeException {
 
-    private static final long serialVersionUID = 616106877599865115L;
+    private static final long serialVersionUID = 3642759996241792935L;
 
     public ProdutoVazioException(String mensagem) {
         super(mensagem);
