64736417ecf903726740c0e2f3e3952f1de75a34
nkutomimeli <nathan.kutomi@mercadolivre.com>
2022-02-09 14:33:11 -0300

    Criação Entidade Veículo

---
+++ src/main/java/com/example/demo/entity/Veiculo.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import com.example.demo.enums.Tipos;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Tipos categoria;
+    private Double volume; // em m3
+    private BigDecimal fretePorLitro;
+
+}
