417b6128caedfe4fd09adeac380c6616b8544564
nkutomimeli <nathan.kutomi@mercadolivre.com>
2022-02-10 14:55:12 -0300

    Atualização do JavaDoc

--- src/main/java/com/example/demo/controller/FreteController.java
+++ src/main/java/com/example/demo/controller/FreteController.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller para Cálculo de frete de carrinho
+ */
 @RestController
 @RequestMapping("api/v1/fresh-products")
 public class FreteController {
